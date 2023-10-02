95a9e0584b9811d543b1e82e7d823f6afbbefff2
Thomas Tucker <elteano@protonmail.com>
2023-10-02 11:06:25 -0700

    Have some notcurses running.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
 CFLAGS=-g -Og
-LDLIBS=-lcurl -lnotcurses
+LDLIBS=-lcurl -lnotcurses -lnotcurses-core
 
 creader : source/creader.o
 	${CC} ${CFLAGS} ${LDLIBS} -o $@ $^
--- source/creader.c
+++ source/creader.c
@@ -1,10 +1,15 @@
 // Let's get an RSS reader working with this stuff
 
 #include<errno.h>
+#include<locale.h>
 #include<stdio.h>
 #include<stdlib.h>
 
 #include<curl/curl.h>
+
+#include<notcurses/notcurses.h>
+
+#define PERR(text) fprintf(stderr, "%s\n", text)
 
 FILE * geturl(char* url)
 {
@@ -43,30 +48,45 @@ FILE * geturl(char* url)
 
 int main(int argc, char** argv)
 {
-  CURL * curl = curl_easy_init();
-  if (curl)
+  struct notcurses * nc_handle;
+  struct ncplane * std_plane;
+  ncinput val;
+
+  // Set locale to appropriate environment locale ; notcurses requires this
+  setlocale(LC_ALL, "");
+
+  // Initialize NotCurses with defaults
+  nc_handle = notcurses_init(NULL, NULL);
+  if (nc_handle)
   {
-    CURLcode result;
-    FILE * tmp = tmpfile();
-    curl_easy_setopt(curl, CURLOPT_URL, "https://dvorak.substack.com/feed");
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, tmp);
-    result = curl_easy_perform(curl);
-    printf("result is %d\n", result);
+    std_plane = notcurses_stdplane(nc_handle);
+    ncplane_set_bg_rgb(std_plane, 0);
+    ncplane_putstr_yx(std_plane, 5, 10, "Let's test some draw!");
 
-    // Now read back from the temporary file
-    rewind(tmp);
-    char * buf = malloc(BUFSIZ);
-    while(!feof(tmp))
+    notcurses_render(nc_handle);
+    // Wait for input
+    // First wait is to "clear" the input as it seems to fire too quickly
+    notcurses_get_blocking(nc_handle, &val);
+    notcurses_get_blocking(nc_handle, &val);
+    do
     {
-      fread(buf, sizeof(*buf), BUFSIZ, tmp);
-      printf("%s", buf);
+      notcurses_get_blocking(nc_handle, &val);
+      if (val.evtype == NCTYPE_PRESS || val.evtype == NCTYPE_REPEAT)
+      {
+        // Only process the key down, not the key up
+        ncplane_putchar(std_plane, val.id);
+        notcurses_render(nc_handle);
+      }
     }
-    putchar('\n');
-    fflush(stdout);
+    while(val.id != NCKEY_ENTER);
 
-    // Clean up operations
-    free(buf);
-    fclose(tmp);
-    curl_easy_cleanup(curl);
+    // Close NotCurses
+    notcurses_stop(nc_handle);
+    // Give some output on the character we received
+    printf("Found char %c\n", val.id);
+  }
+  else
+  {
+    PERR("Error initializing notcurses.");
   }
 }
